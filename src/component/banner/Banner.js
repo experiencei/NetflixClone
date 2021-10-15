2a3405fe43a4fa76398101a0ff72ef792a942e36
experiencei <ayelojahighbee01@gmail.com>
2021-10-15 13:10:55 +0100

    using tmdb api to pupulate the row with images

--- src/component/banner/Banner.js
+++ src/component/banner/Banner.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState , useEffect} from 'react';
 import "./Banner.css";
+import axios from '../axios/axios';
+import requests from '../netflix/requests';
 
 function Banner() {
+    const [ movie , setMovie] = useState([]);
+       useEffect(() => {
+          async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals)
+           setMovie(request.data.results[
+               Math.floor(Math.random() * request.data.results.length)
+           ])
+           return request
+          }
+           fetchData();
+       }, [])
     return (
-        <div>
+        <header>
             
-        </div>
+        </header>
     )
 }
 
