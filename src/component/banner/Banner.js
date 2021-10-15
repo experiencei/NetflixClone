24b2e646c06a93a61ee13f944d06bc6b677403f6
experiencei <ayelojahighbee01@gmail.com>
2021-10-15 13:24:42 +0100

    banner component implementation

--- src/component/banner/Banner.js
+++ src/component/banner/Banner.js
@@ -26,6 +26,12 @@ function Banner() {
             <h1>
                 {movie?.title || movie?.name || movie?.original_name}
             </h1>
+            <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+            </div>
+            <h1 className="banner__description">{movie?.overview}</h1>
+            
            </div>
         </header>
     )
