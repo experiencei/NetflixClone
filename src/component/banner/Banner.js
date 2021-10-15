756b230adc0bbf890bacb5d3e98dbb72e21889aa
experiencei <ayelojahighbee01@gmail.com>
2021-10-15 13:53:08 +0100

    banner component styling with styled component

--- src/component/banner/Banner.js
+++ src/component/banner/Banner.js
@@ -36,6 +36,7 @@ function Banner() {
             <h1 className="banner__description">{truncate(movie?.overview , 150)}</h1>
             
            </div>
+           <div className="banner__fadeBottom" />
         </header>
     )
 }
