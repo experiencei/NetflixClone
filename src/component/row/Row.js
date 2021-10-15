a6185272558dfb30e13f7b820e4752b186846719
experiencei <ayelojahighbee01@gmail.com>
2021-10-15 12:58:13 +0100

    using tmdb api to pupulate the row with images

--- src/component/row/Row.js
+++ src/component/row/Row.js
@@ -3,7 +3,7 @@ import axios from "../axios/axios"
 import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/"
-function Row({ title , fetchUrl }) {
+function Row({ title , fetchUrl, isLargeRow}) {
   const [ movies , setMovies] = useState([]);
 
   useEffect(() => {
@@ -21,8 +21,10 @@ function Row({ title , fetchUrl }) {
             <h2>{title}</h2>
             <div className="row__posters">
               {movies.map( movie => (
-                  <img className="row__poster" 
-                  src={`${base_url}${movie.poster_path}`}
+                  <img
+                  key={movie.id}
+                  className={`row__poster ${isLargeRow  && "row__posterLarge"}`} 
+                  src={`${base_url}${ isLargeRow ? movie.poster_path : movie.backdrop_path}`}
                   alt={movie.name}/>
               ))}
             </div>
